1353c7de1b31c3a2add85431f5f5722b30706c1a
bin <phphat.phanhongphat@gmail.com>
2019-03-15 22:20:11 +0200

    update 2

--- Person.php
+++ Person.php
@@ -5,6 +5,7 @@ abstract class Person
 	private $firstName;
 	private $lastName;
 	private $yearOfBirth;
+	private $age;
 
  public function setfirstName($value)
  {
@@ -33,6 +34,16 @@ abstract class Person
   public function getyearOfBirth()
  {
   return $this->yearOfBirth;
+ }
+
+ public function setage($value)
+ {
+    $this->age=$value;
+ }
+
+  public function getage()
+ {
+  return date('Y')-$this->age;
  }
 
 }
--- index.php
+++ index.php
@@ -12,12 +12,14 @@ $Students = new Students();
 $Students->setfirstName('Hong Phat');
 $Students->setlastName('Phan');
 $Students->setyearOfBirth('1991');
-$Students->setselectedCourses('PHP and Database');
+$Students->setage('1991');
+$Students->setselectedCourses('$setselectedCourses');
 //$Students->setselectedCourses('Database and Javascript');
 $Students->setgreditPoint('3');
 
 echo 'The Student Name is '.$Students->getlastName().' '.$Students->getfirstName().'<br>';
-echo 'Year of Birth : '.$Students->getyearOfBirth();
+echo 'Year of Birth : '.$Students->getyearOfBirth().'<br>';
+echo 'Age is : '.$Students->getage();
 echo '<br>The Course which is selected :  ';
 //$course=$Students->getselectedCourses();
 //print_r($course[2]);
@@ -30,11 +32,13 @@ $Teacher = new Teacher();
 $Teacher->setfirstName('Pekka');
 $Teacher->setlastName('Alaluukas');
 $Teacher->setyearOfBirth('1965.');
+$Teacher->setage('1965');
 $Teacher->setteachingSubjects('PHP and Database');
 $Teacher->setdepartment('OULU, FINLAND.');
 
 echo 'The teacher Name is '.$Teacher->getlastName().' '.$Teacher->getfirstName().'<br>';
-echo 'Year of Birth : '.$Teacher->getyearOfBirth();
+echo 'Year of Birth : '.$Teacher->getyearOfBirth().'<br>';
+echo 'Age is : '.$Teacher->getage();
 echo '<br>The Course which is taught :  ';
 $courses=$Teacher->getteachingSubjects();
 echo $courses[0];
@@ -46,10 +50,12 @@ $Staff = new Staff();
 $Staff->setfirstName('Dinh Tai');
 $Staff->setlastName('Nguyen');
 $Staff->setyearOfBirth('1997.');
+$Staff->setage('1997');
 $Staff->setvacancy('Cleaner');
 
 echo 'The staff name is '.$Staff->getlastName().' '.$Staff->getfirstName().'<br>';
-echo 'Year of Birth : '.$Staff->getyearOfBirth();
+echo 'Year of Birth : '.$Staff->getyearOfBirth().'<br>';
+echo 'Age is : '.$Staff->getage();
 echo '<br>His position : '.$Staff->getvacancy();
 //Staff 2
 echo '<h2>Staff 2</h2>';
@@ -57,8 +63,10 @@ $Staff = new Staff();
 $Staff->setfirstName('Thanh Dat.');
 $Staff->setlastName('Phan');
 $Staff->setyearOfBirth('1996.');
+$Staff->setage('1996');
 $Staff->setvacancy('Caretaker.');
 echo 'The staff name is '.$Staff->getlastName().' '.$Staff->getfirstName().'<br>';
-echo 'Year of Birth : '.$Staff->getyearOfBirth();
+echo 'Year of Birth : '.$Staff->getyearOfBirth().'<br>';
+echo 'Age is : '.$Staff->getage();
 echo '<br>His position : '.$Staff->getvacancy();
 ?>
